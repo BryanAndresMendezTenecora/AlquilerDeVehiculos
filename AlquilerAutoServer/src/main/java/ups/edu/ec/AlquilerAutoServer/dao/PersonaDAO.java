641315ab5e2477f410a989052e3b35a116c13229
Bryan Méndez <47094533+BryanAndresMendezTenecora@users.noreply.github.com>
2022-01-07 13:17:36 -0500

    Configuraciones-Independencias
    DAO,0N,POST CONSTRUCT

--- AlquilerAutoServer/src/main/java/ups/edu/ec/AlquilerAutoServer/dao/PersonaDAO.java
+++ AlquilerAutoServer/src/main/java/ups/edu/ec/AlquilerAutoServer/dao/PersonaDAO.java
@@ -1,5 +1,45 @@
 package ups.edu.ec.AlquilerAutoServer.dao;
 
-public class PersonaDAO {
+import java.util.List;
 
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import ups.edu.ec.AlquilerAutoServer.modelo.Persona;
+
+@Stateless
+public class PersonaDAO {
+	@PersistenceContext
+	private EntityManager em;
+	
+	public void insert(Persona persona) throws Exception {
+		em.persist(persona);
+	}
+	
+	public void update(Persona persona) throws Exception{
+		em.merge(persona);
+	}
+	
+	public Persona read(String cedula) throws Exception {
+		Persona p = em.find(Persona.class, cedula);
+		return p;
+	}
+	
+	public void delete(String cedula) throws Exception {
+		Persona p = em.find(Persona.class, cedula);
+		em.remove(p);
+	}
+
+	public List<Persona> getPersonas() throws Exception{
+		String jpql2="SELECT p FROM Persona p";
+		
+		//nombre= nombre+"%";
+		Query query= em.createQuery(jpql2, Persona.class);
+		//query.setParameter(1, nombre);
+		
+		List<Persona> personas=query.getResultList();
+		return personas;
+	}
 }
--- AlquilerAutoServer/src/main/java/ups/edu/ec/AlquilerAutoServer/on/PersonaON.java
+++ AlquilerAutoServer/src/main/java/ups/edu/ec/AlquilerAutoServer/on/PersonaON.java
@@ -1,5 +1,31 @@
 package ups.edu.ec.AlquilerAutoServer.on;
 
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+
+import ups.edu.ec.AlquilerAutoServer.dao.PersonaDAO;
+import ups.edu.ec.AlquilerAutoServer.modelo.Persona;
+
+@Stateless
 public class PersonaON {
 
+	@Inject
+	private PersonaDAO personaDAO;
+	
+	public void insertarPersona(Persona persona) throws Exception {
+		personaDAO.insert(persona);
+	}
+	
+	
+	public void actualizarPersona(Persona persona) throws Exception {
+		personaDAO.update(persona);
+	}
+	
+	public Persona buscarPersona(String cedula) throws Exception {
+		return personaDAO.read(cedula);
+	}
+	
+	public void eliminarPersona(String cedula) throws Exception {
+		personaDAO.delete(cedula);
+	}
 }
