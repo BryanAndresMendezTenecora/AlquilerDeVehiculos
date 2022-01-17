0c01b7117c30b3e023f0fe562a2ff15bb4c8e287
Bryan Méndez <47094533+BryanAndresMendezTenecora@users.noreply.github.com>
2022-01-17 14:24:10 -0500

    Update PersonaONLocal.java

--- AlquilerAutoServer/src/main/java/ups/edu/ec/AlquilerAutoServer/on/PersonaONLocal.java
+++ AlquilerAutoServer/src/main/java/ups/edu/ec/AlquilerAutoServer/on/PersonaONLocal.java
@@ -1,14 +1,15 @@
 package ups.edu.ec.AlquilerAutoServer.on;
 
+import java.util.List;
+
 import javax.ejb.Local;
 
 import ups.edu.ec.AlquilerAutoServer.modelo.Persona;
-
 @Local
-public interface PersonaONLocal {
-
+public interface PersonaONlocal {
 	public void insertarPersona(Persona persona) throws Exception;
 	public void actualizarPersona(Persona persona) throws Exception;
 	public Persona buscarPersona(String cedula) throws Exception;
 	public void eliminarPersona(String cedula) throws Exception;
+	public List<Persona> getPersonas();
 }
