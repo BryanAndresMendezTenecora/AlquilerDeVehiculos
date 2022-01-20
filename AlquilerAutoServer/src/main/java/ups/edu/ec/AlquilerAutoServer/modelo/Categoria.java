97886fa3b6dd61133817d47dd540712f1df89937
juanboni <52544365+juanboni@users.noreply.github.com>
2022-01-19 21:03:35 -0500

    categoria

--- AlquilerAutoServer/src/main/java/ups/edu/ec/AlquilerAutoServer/modelo/Categoria.java
+++ AlquilerAutoServer/src/main/java/ups/edu/ec/AlquilerAutoServer/modelo/Categoria.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -12,6 +14,7 @@ import javax.persistence.Table;
 public class Categoria implements Serializable {
 	private static final long serialVersionUID = 1L;
 	@Id
+	@GeneratedValue(strategy = GenerationType.TABLE)
 	@Column(name = "cat_id")
 	private int id;
 	private String nombre;
