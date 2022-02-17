a8dea1de1200db894be45f0064aa552537250cc3
braulio97x <52545107+braulio97x@users.noreply.github.com>
2022-02-17 01:33:53 -0500

    documentacion modelo

--- AlquilerAutoServer/src/main/java/ups/edu/ec/AlquilerAutoServer/modelo/MetodoDePago.java
+++ AlquilerAutoServer/src/main/java/ups/edu/ec/AlquilerAutoServer/modelo/MetodoDePago.java
@@ -1,7 +1,6 @@
 package ups.edu.ec.AlquilerAutoServer.modelo;
 
 import java.io.Serializable;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,78 +8,194 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
+
+/**
+ * Modelo que servira como entidad para el metodo de pago
+ * 
+ * @author Bryan Mendez
+ * @author Braulio Astudillo
+ * @author Juan Boni
+ *
+ */
 @Entity
 @Table(name = "TBL_MetodoPago")
-public class MetodoDePago implements Serializable{
-	private static final long serialVersionUID = 1L;
+public class MetodoDePago implements Serializable {
+	private static final long serialVersionUID = 1L; // Numero de versión
 	@Id
 	@Column(name = "tar_id")
 	@SequenceGenerator(name = "id_met_seq", sequenceName = "ID_MET_SEQ", initialValue = 1, allocationSize = 1)
-	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator = "id_met_seq")
-	private int id;
-	
-	private String tipo;
-	private String numero;
-	private String nombrepropietario;
-	private String fechavencimiento;
-	private int codigoseguridad;
-	private String direccion;
-	private String codigoPostal;
-	private String estado;
-	//private String banco;
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "id_met_seq")
+	private int id; // LLava primaria de la tarjeta
+
+	private String tipo; // Tipo de tarjeta
+	private String numero; // Numero de la tarjeta
+	private String nombrepropietario; // Nombre del titular de la tarjeta
+	private String fechavencimiento; // Fecha de vencimiento de la tarjeta
+	private int codigoseguridad; // Codigo CVV de la tarjeta
+	private String direccion; // Direccion para facturacion
+	private String codigoPostal; // Codigo postal para la facturacion
+	private String estado; // Estado de la tarjeta (Activo, Inactivo)
+
+	/**
+	 * Devuelve el id del método de pago
+	 * 
+	 * @return devuelve la clave primaria
+	 */
 	public int getId() {
 		return id;
 	}
+
+	/**
+	 * Asigna el id del método de pago
+	 * 
+	 * @param recibe el codigo
+	 */
 	public void setId(int id) {
 		this.id = id;
 	}
+
+	/**
+	 * Devuelve el tipo del método de pago
+	 * 
+	 * @return devuelve el tipo
+	 */
 	public String getTipo() {
 		return tipo;
 	}
+
+	/**
+	 * Asigna el tipo del método de pago
+	 * 
+	 * @param tipo recibe el tipo
+	 */
 	public void setTipo(String tipo) {
 		this.tipo = tipo;
 	}
+
+	/**
+	 * Devuelve el nombre del titular del método de pago
+	 * 
+	 * @return devuelve el nombre
+	 */
 	public String getNombrepropietario() {
 		return nombrepropietario;
 	}
+
+	/**
+	 * Asigna el nombre del titular del método de pago
+	 * 
+	 * @param nombrepropietario recibe el nombre
+	 */
 	public void setNombrepropietario(String nombrepropietario) {
 		this.nombrepropietario = nombrepropietario;
 	}
+
+	/**
+	 * Devuelve la fecha de vencimiento del metodo de paago
+	 * 
+	 * @return devuelve la fecha de vigencia
+	 */
 	public String getFechavencimiento() {
 		return fechavencimiento;
 	}
+
+	/**
+	 * Asigna la fecha de vencimiento del metodo de pago
+	 * 
+	 * @param fechavencimiento recibe la fecha de vencimiento
+	 */
 	public void setFechavencimiento(String fechavencimiento) {
 		this.fechavencimiento = fechavencimiento;
 	}
+
+	/**
+	 * Devuelve el codigo CVV del metodo de pago
+	 * 
+	 * @return devuelve el codigo CVV
+	 */
 	public int getCodigoseguridad() {
 		return codigoseguridad;
 	}
+
+	/**
+	 * Asigna el codigo CVV del metodo de pago
+	 * 
+	 * @param codigoseguridad recibe el codigo CVV
+	 */
 	public void setCodigoseguridad(int codigoseguridad) {
 		this.codigoseguridad = codigoseguridad;
 	}
+
+	/**
+	 * Devuelve la direccion del metodo de pago
+	 * 
+	 * @return devuelve la direccion
+	 */
 	public String getDireccion() {
 		return direccion;
 	}
+
+	/**
+	 * Asigna la direccion de facturacion del metodo de pago
+	 * 
+	 * @param direccion recibe la direccion
+	 */
 	public void setDireccion(String direccion) {
 		this.direccion = direccion;
 	}
+
+	/**
+	 * Devuelve el codigo postal para la facturacion del metodo de pago
+	 * 
+	 * @return devuelve el codigo postal
+	 */
 	public String getCodigoPostal() {
 		return codigoPostal;
 	}
+
+	/**
+	 * Asigna el codigo postal del metodo de pago
+	 * 
+	 * @param codigoPostal recibe el codigo postal
+	 */
 	public void setCodigoPostal(String codigoPostal) {
 		this.codigoPostal = codigoPostal;
 	}
+
+	/**
+	 * Devuelve el estado del metodo de pago
+	 * 
+	 * @return devuelve el estado
+	 */
 	public String getEstado() {
 		return estado;
 	}
+
+	/**
+	 * Asigna el estado del metodo de pago
+	 * 
+	 * @param estado recibe el estado
+	 */
 	public void setEstado(String estado) {
 		this.estado = estado;
 	}
+
+	/**
+	 * Devuelve el numero del metod de pago
+	 * 
+	 * @return devuelve el numero
+	 */
 	public String getNumero() {
 		return numero;
 	}
+
+	/**
+	 * Asigna el numero del metodo de pago
+	 * 
+	 * @param numero recibe el numero
+	 */
 	public void setNumero(String numero) {
 		this.numero = numero;
 	}
-	
+
 }
--- AlquilerAutoServer/src/main/java/ups/edu/ec/AlquilerAutoServer/modelo/Vehiculo.java
+++ AlquilerAutoServer/src/main/java/ups/edu/ec/AlquilerAutoServer/modelo/Vehiculo.java
@@ -13,68 +13,157 @@ import javax.persistence.OneToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
+/**
+ * Modelo que servira como entidad para vehiculo
+ * 
+ * @author Bryan Mendez
+ * @author Braulio Astudillo
+ * @author Juan Boni
+ *
+ */
 @Entity
 @Table(name = "TBL_vehiculo")
-public class Vehiculo implements Serializable{
-	private static final long serialVersionUID = 1L;
+public class Vehiculo implements Serializable {
+	private static final long serialVersionUID = 1L; // Numero de versión
 	@Id
-	//@GeneratedValue(strategy = GenerationType.TABLE)
 	@Column(name = "veh_id")
 	@SequenceGenerator(name = "id_veh_seq", sequenceName = "ID_VEH_SEQ", initialValue = 1, allocationSize = 1)
-	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator = "id_veh_seq")
-	private int id;
-	private String estado;
-	private String modelo;
-	private String marca;
-	private int stock;
-	private double precio;
-	
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "id_veh_seq")
+	private int id; // Llave primaria de vehiculo
+	private String estado; // Estado del vehiculo (disponible, alquilado, mantenimiento, eliminado)
+	private String modelo; // Modelo del vehiculo
+	private String marca; // Marca del vehiculo
+	private int stock; // Stock del vehiculo
+	private double precio; // Precio del vehiculo
+
 	@OneToOne
 	@JoinColumn(name = "cat_id")
-	private Categoria categoria;
+	private Categoria categoria; // Categoria del vehiculo
+
+	/**
+	 * Devuelve el id del vehiculo
+	 * 
+	 * @return devuelve la clave primaria
+	 */
 	public int getId() {
 		return id;
 	}
+
+	/**
+	 * Asigna el id del vehículo
+	 * 
+	 * @param id recibe el codigo
+	 */
 	public void setId(int id) {
 		this.id = id;
 	}
+
+	/**
+	 * Devuelve el estado del vehículo
+	 * 
+	 * @return devuelve el estado (disponible, alquilado, mantenimiento, eliminado)
+	 */
 	public String getEstado() {
-		return estado;
+		return estado; // estado de ese vehiculo
 	}
+
+	/**
+	 * Asigna el estado del vehículo
+	 * 
+	 * @param estado recibe el estado
+	 */
 	public void setEstado(String estado) {
 		this.estado = estado;
 	}
+
+	/**
+	 * Devuelve el modelo del vehículo
+	 * 
+	 * @return devuelve el modelo
+	 */
 	public String getModelo() {
 		return modelo;
 	}
+
+	/**
+	 * Asigna el modelo del vehículo
+	 * 
+	 * @param modelo recibe el modelo
+	 */
 	public void setModelo(String modelo) {
 		this.modelo = modelo;
 	}
+
+	/**
+	 * Devuelve la marca del vehículo
+	 * 
+	 * @return devuelve la marca
+	 */
 	public String getMarca() {
 		return marca;
 	}
+
+	/**
+	 * Asigna la marca del vehículo
+	 * 
+	 * @param marca recibe la marca
+	 */
 	public void setMarca(String marca) {
 		this.marca = marca;
 	}
+
+	/**
+	 * Devuelve el stock del vehículo
+	 * 
+	 * @return devuelve el stock
+	 */
 	public int getStock() {
 		return stock;
 	}
+
+	/**
+	 * Asigna el stock al vehículo
+	 * 
+	 * @param stock recibe el stock
+	 */
 	public void setStock(int stock) {
 		this.stock = stock;
 	}
+
+	/**
+	 * Devuelve la categoria del vehículo
+	 * 
+	 * @return devuelve la categoria
+	 */
 	public Categoria getCategoria() {
 		return categoria;
 	}
+
+	/**
+	 * Asigna la categoria del vehiculo
+	 * 
+	 * @param categoria recibe la categoria
+	 */
 	public void setCategoria(Categoria categoria) {
 		this.categoria = categoria;
 	}
+
+	/**
+	 * Devuelve el precio del vehículo
+	 * 
+	 * @return devuelve el precio
+	 */
 	public double getPrecio() {
 		return precio;
 	}
+
+	/**
+	 * Asigna el precio del vehículo
+	 * 
+	 * @param precio recibe el precio del vehiculo
+	 */
 	public void setPrecio(double precio) {
 		this.precio = precio;
 	}
-	
-	
-	
+
 }
