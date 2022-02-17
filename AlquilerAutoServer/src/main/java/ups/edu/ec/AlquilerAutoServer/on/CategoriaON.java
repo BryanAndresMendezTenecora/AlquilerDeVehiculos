95e66a1c93f04727db809388bec6c855472d1fa8
braulio97x <52545107+braulio97x@users.noreply.github.com>
2022-02-17 03:41:49 -0500

    categoriaOn

--- AlquilerAutoServer/src/main/java/ups/edu/ec/AlquilerAutoServer/on/CategoriaON.java
+++ AlquilerAutoServer/src/main/java/ups/edu/ec/AlquilerAutoServer/on/CategoriaON.java
@@ -10,23 +10,35 @@ import ups.edu.ec.AlquilerAutoServer.dao.CategoriaDAO;
 import ups.edu.ec.AlquilerAutoServer.modelo.Categoria;
 import ups.edu.ec.AlquilerAutoServer.modelo.Persona;
 
+/**
+ * Objeto de negocio de Categoria 
+ * 
+ * @author Bryan Mendez
+ * @author Braulio Astudillo
+ * @author Juan Boni
+ *
+ */
 @Stateless
 public class CategoriaON implements CategoriaONLocal{
-	@Inject
+	@Inject //Se inyecta el objeto de acceso a datos de categoria
 	private CategoriaDAO categoriaDAO;
 	
+	/**
+	 * Metodo Insertar Categoria
+	 * @param categoria recibe el objeto categoria
+	 * @throws Exception para capturar excepciones
+	 */
 	public void insertarCategoria(Categoria categoria) throws Exception {
-		/*String estado=categoria.getEstado().toUpperCase();
-		String nombre=categoria.getNombre().toUpperCase();
-		categoria.setNombre(nombre);
-		categoria.setEstado(estado);
-		*/
 		categoria.setEstado(categoria.getEstado().toUpperCase());
 		categoria.setNombre(categoria.getNombre().toUpperCase());
 		categoriaDAO.insert(categoria);
 	}
 	
-	
+	/**
+	 * Metodo actualizar Categoria
+	 * @param categoria recibe el objeto
+	 * @throws Exception para capturar excepciones
+	 */
 	public void actualizarCategoria(Categoria categoria) throws Exception {
 		String estado=categoria.getEstado().toUpperCase();
 		String nombre=categoria.getNombre().toUpperCase();
@@ -35,25 +47,65 @@ public class CategoriaON implements CategoriaONLocal{
 		categoriaDAO.update(categoria);
 	}
 	
+	/**
+	 * Metodo buscar Categoria
+	 * @param id recibe el id de la categoria
+	 * @return devuelve el objeto categoria encontrado
+	 * @throws Exception para capturar excepciones
+	 */
 	public Categoria buscarCategoria(int id) throws Exception {
 		return categoriaDAO.read(id);
 	}
 	
+	
+	/**
+	 * Metodo eliminar Categoria
+	 * @param id recibe el id de la categoria
+	 * @throws Exception para capturar excepciones
+	 */
 	public void eliminarCategoria(int id) throws Exception {
 		categoriaDAO.delete(id);
 	}
+	
+	/**
+	 * Metodo que devuelve una lista de categorias
+	 * 
+	 * @return devuelve la lista
+	 */
 	public List<Categoria> getCategorias() {
 		return categoriaDAO.getList();
 	}
 	
+	
+	/**
+	 * Metodo que devuelve una lista de categorias por nombre
+	 * 
+	 * @param categoria recibe el nombre
+	 * @return devuelve la lista
+	 * @throws Exception para capturar excepciones
+	 */
 	public Categoria buscarNombre(String categoria) throws Exception{
 		categoria.toUpperCase();
 		return categoriaDAO.buscarNombre(categoria);
 	}
+	
+	/**
+	 * Metodo que devuelve una lista de categorias por id
+	 * 
+	 * @param id recibe el id
+	 * @return devuelve la lista
+	 * @throws Exception para capturar excepciones
+	 */
 	public Categoria getCategoria(int id) throws Exception {
 		return categoriaDAO.read(id);
 	}
 	
+	
+	/**
+	 * Metodo para guardar o actualizar una categoria
+	 * @param p recibe el objeto
+	 * @throws Exception para capturar excepciones
+	 */
 	public void guardar(Categoria p) throws Exception {
 		System.out.println("ON: CO-CATE"+p.getId());
 		p.setEstado(p.getEstado().toUpperCase());
@@ -68,6 +120,11 @@ public class CategoriaON implements CategoriaONLocal{
 			
 	}
 	
+	
+	/**
+	 * Metodo que devuelve una lista de categorias
+	 * @return devuelve una lista
+	 */
 	public List<Categoria> listarcategorias(){
 		
 		return categoriaDAO.listarcategorias();
--- AlquilerAutoServer/src/main/java/ups/edu/ec/AlquilerAutoServer/on/CategoriaONLocal.java
+++ AlquilerAutoServer/src/main/java/ups/edu/ec/AlquilerAutoServer/on/CategoriaONLocal.java
@@ -5,18 +5,85 @@ import java.util.List;
 import javax.ejb.Local;
 
 import ups.edu.ec.AlquilerAutoServer.modelo.Categoria;
+
+/**
+ * Interfaz local del objeto de negocio Categoria 
+ * 
+ * @author Bryan Mendez
+ * @author Braulio Astudillo
+ * @author Juan Boni
+ *
+ */
 @Local
 public interface CategoriaONLocal {
+	/**
+	 * Declaracion del metodo Insertar Categoria
+	 * @param categoria recibe el objeto categoria
+	 * @throws Exception para capturar excepciones
+	 */
 	public void insertarCategoria(Categoria categoria) throws Exception;
+	
+	/**
+	 * Declaracion del metodo actualizar Categoria
+	 * @param categoria recibe el objeto
+	 * @throws Exception para capturar excepciones
+	 */
 	public void actualizarCategoria(Categoria categoria) throws Exception;
+	
+	/**
+	 * Declaracion del metodo buscar Categoria
+	 * @param id recibe el id de la categoria
+	 * @return devuelve el objeto categoria encontrado
+	 * @throws Exception para capturar excepciones
+	 */
 	public Categoria buscarCategoria(int id) throws Exception;
+	
+	/**
+	 * Declaracion del metodo eliminar Categoria
+	 * @param id recibe el id de la categoria
+	 * @throws Exception para capturar excepciones
+	 */
 	public void eliminarCategoria(int id) throws Exception;
+	
+	/**
+	 * Declaracion del metodo que devuelve una lista de categorias
+	 * 
+	 * @return devuelve la lista
+	 */
 	public List<Categoria> getCategorias();
+	
+	/**
+	 * Declaracion del metodo que devuelve una lista de categorias por nombre
+	 * 
+	 * @param categoria recibe el nombre
+	 * @return devuelve la lista
+	 * @throws Exception para capturar excepciones
+	 */
 	public Categoria buscarNombre(String categoria) throws Exception;
+	
+	
+	/**
+	 * Declaracion del metodo que devuelve una lista de categorias por id
+	 * 
+	 * @param id recibe el id
+	 * @return devuelve la lista
+	 * @throws Exception para capturar excepciones
+	 */
 	public Categoria getCategoria(int id) throws Exception;
 	
+	
+	/**
+	 * Declaracion del metodo para guardar una categoria
+	 * @param p recibe el objeto
+	 * @throws Exception para capturar excepciones
+	 */
 	public void guardar(Categoria p) throws Exception;
 	
+	
+	/**
+	 * Declaracion del metodo que devuelve una lista de categorias
+	 * @return devuelve una lista
+	 */
 	public List<Categoria> listarcategorias();
 	
 	
