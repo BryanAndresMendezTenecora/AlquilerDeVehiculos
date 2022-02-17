bf40f0bbed4f344d6eb45919fd035b4152d00af3
Bryan Méndez <47094533+BryanAndresMendezTenecora@users.noreply.github.com>
2022-02-17 03:09:42 -0500

    comentario-carrito

--- AlquilerAutoServer/src/main/java/ups/edu/ec/AlquilerAutoServer/bean/MetodoDePagoBean.java
+++ AlquilerAutoServer/src/main/java/ups/edu/ec/AlquilerAutoServer/bean/MetodoDePagoBean.java
@@ -29,43 +29,65 @@ public class MetodoDePagoBean implements Serializable{
 	private Date date;  // Atributo donde se le guardara la información de la fecha actual
 
 	/**
-	 * Recuperación de un objeto tipo Date para la fecha actual
-	 * @return devuelve un objeto tipo Date
+	 * Recuperación de un objeto tipo Date para la fecha actual.
+	 * @return devuelve un objeto tipo Date.
 	 */
 	public Date getDate() {
 		return date;
 	}
 
 	/**
-	 * 
-	 * @param date
+	 * Asigna un objeto tipo Date.
+	 * @param El parámentro date tendra información de la fecha actual
 	 */
 	public void setDate(Date date) {
 		this.date = date;
 	}
 
+	/**
+	 * Se encarga de recuperar todos los objetos tipo MetodoDePago.
+	 */
 	@PostConstruct
 	public void init() {
 		this.loadPagos();
 	}
 
-
+	/**
+	 * Recuperación de un objeto tipo MetodoDePago.
+	 * @return devuelve un objeto tipo MetodoDePago.
+	 */
 	public MetodoDePago getPago() {
 		return pago;
 	}
 
+	/**
+	 * Asigna un objeto tipo MetodoDePago.
+	 * @param El parámetro pago se asignara al objeto.
+	 */
 	public void setmetodoPago(MetodoDePago pago) {
 		this.pago = pago;
 	}
 
+	/**
+	 * Recuperación de una lista tipo MetodoDePago.
+	 * @return devuelve una lista de tipo MetodoDePago
+	 */
 	public List<MetodoDePago> getPagos() {
 		return metodosPagos;
 	}
 
+	/**
+	 * Asigna una lista de metodos pagos
+	 * @param metodosPagos es el parametro que contiene todos los metodos de pago.
+	 */
 	public void setPagos(List<MetodoDePago> metodosPagos) {
 		this.metodosPagos = metodosPagos;
 	}
 
+	/**
+	 * Método que insertara o actualizara el objeto metodo de pago.
+	 * @return Me permite la navegación a otra página.
+	 */
 	public String guardar() {
 
 		System.out.println("Guardando " + this.pago.getNombrepropietario());
@@ -81,22 +103,35 @@ public class MetodoDePagoBean implements Serializable{
 		return "listarPago?faces-redirect=true";
 	}
 
+	/**
+	 * Permita la edición del objeto mediante un código que es la llave la primaria.
+	 * @param codigo es el parámetro, pertenece a una llave primaria.
+	 * @return Me permite la navegación a otra página.
+	 */
 	public String editar(int codigo) {
 		System.out.println(codigo);
 		return "crear-pago?faces-redirect=true&id=" + codigo;
 	}
 
-	
-
+	/**
+	 * Recuperación de un dato
+	 * @return devuelve un dato tipo int.
+	 */
 	public int getCodigo() {
 		return codigo;
 	}
 
+	/**
+	 * Asigna un dato a un objeto.
+	 * @param El parámetro codigo que se asignara al objeto 
+	 */
 	public void setCodigo(int codigo) {
-		System.out.println("set " + codigo);
 		this.codigo = codigo;
 	}
 
+	/**
+	 * Recuperación de uan lista sobre los metodo de pago
+	 */
 	private void loadPagos() {
 		try {
 			this.metodosPagos = pagosON.getMetodoPagos();
@@ -107,6 +142,9 @@ public class MetodoDePagoBean implements Serializable{
 	}
 	
 	
+	/**
+	 * Metodo que se encarga de recuperar un Objeto de método de pago.
+	 */
 	public void loadData() {
 		if(codigo==0)
 			return;
@@ -122,7 +160,9 @@ public class MetodoDePagoBean implements Serializable{
 		
 	}
 	
-	
+	/**
+	 * Metodo que me permite hacer la edición a un objeto 
+	 */
 	public void loadDataEditar() {
 		MetodoDePago p;
 		try {
@@ -135,7 +175,13 @@ public class MetodoDePagoBean implements Serializable{
 		
 	}
 	
-	//#String ced=c
+	/**
+	 * Permite la edición a un objeto, se actualizara el estado del objeto.
+	 * a eliminado
+	 * @param idTar, es la llave primaria del objeto a buscar.
+	 * @return Me permite la navegación a otra página.
+	 */
+
 	public String eliminar(int idTar) {
 	
 		try {
