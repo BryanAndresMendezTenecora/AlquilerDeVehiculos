2feb2a1b5e4151e474e32d821f6d14a301c96450
juanboni <52544365+juanboni@users.noreply.github.com>
2022-02-17 01:37:24 -0500

    doc java modelo

--- AlquilerAutoServer/src/main/java/ups/edu/ec/AlquilerAutoServer/modelo/Categoria.java
+++ AlquilerAutoServer/src/main/java/ups/edu/ec/AlquilerAutoServer/modelo/Categoria.java
@@ -10,37 +10,78 @@ import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
+/**
+ * Modelo que sirve como entidad para categoria
+ * 
+ * @author Bryan Mendez
+ * @author Braulio Astudillo
+ * @author Juan Boni
+ *
+ */
 @Entity
 @Table(name = "TBL_categoria")
 public class Categoria implements Serializable {
 	private static final long serialVersionUID = 1L;
 	@Id
-	//@GeneratedValue(strategy = GenerationType.TABLE)
 	@SequenceGenerator(name = "id_cat_seq", sequenceName = "ID_CAT_SEQ", initialValue = 1, allocationSize = 1)
-	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator = "id_cat_seq")
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "id_cat_seq")
 	@Column(name = "cat_id")
-	private int id;
-	private String nombre;
-	private String estado;
+	private int id; // Clave primaria de categoria
+	private String nombre;// Nombre de la categoria
+	private String estado;// Estado de la categoria
+
+	/**
+	 * Devuelve id de la categoria
+	 * 
+	 * @return devuelve id
+	 */
 	public int getId() {
 		return id;
 	}
+
+	/**
+	 * Asigna id de la categoria
+	 * 
+	 * @param recibe id
+	 */
 	public void setId(int id) {
 		this.id = id;
 	}
+
+	/**
+	 * Devuelve nombre de la categoria
+	 * 
+	 * @return devuelve nombre
+	 */
 	public String getNombre() {
 		return nombre;
 	}
+
+	/**
+	 * Asigna nombre de la categoria
+	 * 
+	 * @param recibe nombre
+	 */
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+
+	/**
+	 * Devuelve Estado de la categoria
+	 * 
+	 * @return devuelve categoria
+	 */
 	public String getEstado() {
 		return estado;
 	}
+
+	/**
+	 * Asigna estado de la categoria
+	 * 
+	 * @param recibe estado
+	 */
 	public void setEstado(String estado) {
 		this.estado = estado;
 	}
-	
-	
 
 }
--- AlquilerAutoServer/src/main/java/ups/edu/ec/AlquilerAutoServer/modelo/Devolucion.java
+++ AlquilerAutoServer/src/main/java/ups/edu/ec/AlquilerAutoServer/modelo/Devolucion.java
@@ -13,44 +13,100 @@ import javax.persistence.OneToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
+/**
+ * Modelo que sirve como entidad para Devolucion
+ * 
+ * @author Bryan Mendez
+ * @author Braulio Astudillo
+ * @author Juan Boni
+ *
+ */
 @Entity
 @Table(name = "TBL_devolucion")
 public class Devolucion implements Serializable {
-	
+
 	private static final long serialVersionUID = 1L;
 	@Id
 	@Column(name = "deb_id")
-	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator = "id_dev_seq")
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "id_dev_seq")
 	@SequenceGenerator(name = "id_dev_seq", sequenceName = "ID_DEV_SEQ", initialValue = 1, allocationSize = 1)
-	private int id;
-	private String fecha;
-	private String antecedente;
+	private int id;// Clave primaria de la devolucion
+	private String fecha;// Fecha de la factura
+	private String antecedente; // Antecedente del vehiculo al devolver
 	@OneToOne
 	@JoinColumn(name = "fac_id")
-	private Factura factura;
+	private Factura factura;// Factura de la devolucion
+
+	/**
+	 * Devuelve id de la devolucion
+	 * 
+	 * @return devuelve id
+	 */
 	public int getId() {
 		return id;
 	}
+
+	/**
+	 * Asigna id de la devolucion
+	 * 
+	 * @param recibe id
+	 */
 	public void setId(int id) {
 		this.id = id;
 	}
+
+	/**
+	 * Devuelve fecha de la devolucion
+	 * 
+	 * @return devuelve fecha
+	 */
 	public String getFecha() {
 		return fecha;
 	}
+
+	/**
+	 * Asigna Fecha de la devolucion
+	 * 
+	 * @param recibe fecha
+	 */
 	public void setFecha(String fecha) {
 		this.fecha = fecha;
 	}
+
+	/**
+	 * Devuelve antecedente de la devolucion
+	 * 
+	 * @return devuelve antecedente
+	 */
 	public String getAntecedente() {
 		return antecedente;
 	}
+
+	/**
+	 * Asigna antecedente de la devolucion
+	 * 
+	 * @param recibe antecedente
+	 */
 	public void setAntecedente(String antecedente) {
 		this.antecedente = antecedente;
 	}
+
+	/**
+	 * Devuelve Factura de la devolucion
+	 * 
+	 * @return devuelve factura
+	 */
 	public Factura getFactura() {
 		return factura;
 	}
+
+	/**
+	 * Asigna Factura de la devolucion
+	 * 
+	 * @param recibe factura
+	 */
 	public void setFactura(Factura factura) {
 		this.factura = factura;
 	}
-	
+
 }
