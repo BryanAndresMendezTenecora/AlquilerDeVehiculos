df1e757304f20f2a652937c9a3c26b0ffa369e07
juanboni <52544365+juanboni@users.noreply.github.com>
2022-02-17 04:39:54 -0500

    rrmrm

--- AlquilerAutoServer/src/main/java/ups/edu/ec/AlquilerAutoServer/on/MetodoDePagoONLocal.java
+++ AlquilerAutoServer/src/main/java/ups/edu/ec/AlquilerAutoServer/on/MetodoDePagoONLocal.java
@@ -3,17 +3,76 @@ package ups.edu.ec.AlquilerAutoServer.on;
 import java.util.List;
 
 import javax.ejb.Local;
-
 import ups.edu.ec.AlquilerAutoServer.dao.metodoDePagoDAO;
 import ups.edu.ec.AlquilerAutoServer.modelo.MetodoDePago;
+
+/**
+ * Interfaz de Objeto de negocio de metodo de pago
+ * 
+ * @author Bryan Mendez
+ * @author Braulio Astudillo
+ * @author Juan Boni
+ *
+ */
 @Local
 public interface MetodoDePagoONLocal {
 
+	/**
+	 * Declaracion del metodo insertar metodo pago
+	 * 
+	 * @param recibe objeto tarjetaCredito
+	 * @throws Exception para capturar excepciones
+	 */
 	public void insertarMetodoPago(MetodoDePago tarjetaCredito) throws Exception;
+
+	/**
+	 * Declaracion del metodo actualizar
+	 * 
+	 * @param recibe objeto tarjetaCredito
+	 * @throws Exception para capturar excepciones
+	 */
 	public void actualizarMetodoPago(MetodoDePago tarjetaCredito) throws Exception;
+
+	/**
+	 * Declaracion del metodo buscar metodo
+	 * 
+	 * @param recibe id del metodo de pago
+	 * @return devuelve objeto metodo de pago
+	 * @throws Exception para capturar excepciones
+	 */
 	public MetodoDePago buscarMetodoPago(int id) throws Exception;
+
+	/**
+	 * Declaracion del metodo eliminar metodo del pago
+	 * 
+	 * @param recibe id del metodo del pago
+	 * @throws Exception para capturar excepciones
+	 */
 	public void eliminarMetodoPago(int id) throws Exception;
+
+	/**
+	 * Declaracion del metodo de listar metodos de pagos
+	 * 
+	 * @return devuelve una lista de metodos de pagos
+	 * @throws Exception para capturar excepciones
+	 */
 	public List<MetodoDePago> getMetodoPagos() throws Exception;
+
+	/**
+	 * Declaracion del metodo buscar metodo de pago
+	 * 
+	 * @param recibe c como objeto metodoDePago
+	 * @return devuelve una lista de metodos de pagos
+	 * @throws Exception para capturar excepciones
+	 */
 	public MetodoDePago getPago(int c) throws Exception;
+
+	/**
+	 * 
+	 * Declaracion del metodo guardar metodo de pago
+	 * 
+	 * @param recibe p como objeto metodo de pago
+	 * @throws Exception para capturar excepciones
+	 */
 	public void guardar(MetodoDePago p) throws Exception;
 }
--- AlquilerAutoServer/src/main/java/ups/edu/ec/AlquilerAutoServer/on/PedidoONLocal.java
+++ AlquilerAutoServer/src/main/java/ups/edu/ec/AlquilerAutoServer/on/PedidoONLocal.java
@@ -5,6 +5,7 @@ import javax.ejb.Local;
 
 import ups.edu.ec.AlquilerAutoServer.modelo.Persona;
 import ups.edu.ec.AlquilerAutoServer.modelo.pedidoCabecera;
+
 /**
  * Interfaz de Objeto de negocio de pedido
  * 
@@ -15,47 +16,53 @@ import ups.edu.ec.AlquilerAutoServer.modelo.pedidoCabecera;
  */
 @Local
 public interface PedidoONLocal {
-	
+
 	/**
 	 * Declaracion de Metodo para insertar pedido
+	 * 
 	 * @param recibe objeto pedidoCabece
 	 * @throws Exception para capturar excepciones
 	 */
-	public void insertarpedidoCabecera(pedidoCabecera pedidoCabece)throws Exception;
-	
+	public void insertarpedidoCabecera(pedidoCabecera pedidoCabece) throws Exception;
+
 	/**
 	 * Declaracion de metodo para actualizar pedido
+	 * 
 	 * @param recibe objeto pedidoCabece
 	 * @throws Exception para capturar excepciones
 	 */
 	public void actualizarpedidoCabecera(pedidoCabecera pedidoCabece) throws Exception;
-	
+
 	/**
-	 * Declaracion de metodo para buscar pedido 
+	 * Declaracion de metodo para buscar pedido
+	 * 
 	 * @param recibe id de pedido
 	 * @return devuelve objeto pedido
 	 * @throws Exception para capturar excepciones
 	 */
 	public pedidoCabecera buscarpedidoCabecera(int id) throws Exception;
-	
+
 	/**
 	 * Declaracion de metodo eliminar pedido
+	 * 
 	 * @param recibe id de pedido
 	 * @throws Exception para capturar excepciones
 	 */
 	public void eliminarpedidoCabecera(int id) throws Exception;
-	
+
 	/**
 	 * Declaracion de metodo para listar pedido
+	 * 
 	 * @return devuelve una lista de pedidos
 	 */
 	public List<pedidoCabecera> getpedidoCabeceras();
-	
+
 	/**
-	 * Declaracion de metodo  de listar pedidos
+	 * Declaracion de metodo de listar pedidos
+	 * 
 	 * @param recibe un objeto persona
 	 * @return devuelve una lista de pedidos por por persona
 	 */
 	public List<pedidoCabecera> getPedidosCedula(Persona persona);
-	
+
 }
