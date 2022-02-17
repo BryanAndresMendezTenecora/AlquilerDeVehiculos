21e2ecd0112214e84461641957f565c5d3b6847f
braulio97x <52545107+braulio97x@users.noreply.github.com>
2022-02-17 03:56:41 -0500

    comentarioOn

--- AlquilerAutoServer/src/main/java/ups/edu/ec/AlquilerAutoServer/on/ComentarioON.java
+++ AlquilerAutoServer/src/main/java/ups/edu/ec/AlquilerAutoServer/on/ComentarioON.java
@@ -9,32 +9,77 @@ import ups.edu.ec.AlquilerAutoServer.dao.ComentarioDAO;
 
 import ups.edu.ec.AlquilerAutoServer.modelo.Comentario;
 import ups.edu.ec.AlquilerAutoServer.modelo.Vehiculo;
+
+/**
+ * Objeto de negocio de Comentario
+ * 
+ * @author Bryan Mendez
+ * @author Braulio Astudillo
+ * @author Juan Boni
+ *
+ */
 @Stateless
-public class ComentarioON implements ComentarioONLocal{
-	@Inject
+public class ComentarioON implements ComentarioONLocal {
+	@Inject // Se inyecta el objeto de acceso a datos de comentario
 	private ComentarioDAO comentarioDAO;
-	
+
+	/**
+	 * Declaracion del metodo Insertar comentario
+	 * 
+	 * @param comentario recibe el objeto
+	 * @throws Exception
+	 */
 	public void insertarComentario(Comentario comentario) throws Exception {
 		comentarioDAO.insert(comentario);
 	}
-	
-	
+
+	/**
+	 * Declaracion del metodo actualizar comentario
+	 * 
+	 * @param comentario recibe el objeto
+	 * @throws Exception para capturar excepciones
+	 */
 	public void actualizarComentario(Comentario comentario) throws Exception {
 		comentarioDAO.update(comentario);
 	}
-	
+
+	/**
+	 * Declaracion del metodo buscar comentario
+	 * 
+	 * @param id recibe el id
+	 * @return devuelve el objeto comentario encontrado
+	 * @throws Exception para capturar excepciones
+	 */
 	public Comentario buscarComentario(int id) throws Exception {
 		return comentarioDAO.read(id);
 	}
-	
+
+	/**
+	 * Declaracion del metodo eliminar comentario
+	 * 
+	 * @param id recibe el id
+	 * @throws Exception para capturar excepciones
+	 */
 	public void eliminarComentario(int id) throws Exception {
 		comentarioDAO.delete(id);
 	}
-	public List<Comentario> getComentarios(){
+
+	/**
+	 * Declaracion del metodo que devuelve una lista de comentario
+	 * 
+	 * @return devuelve la lista
+	 */
+	public List<Comentario> getComentarios() {
 		return comentarioDAO.getList();
 	}
-	
-	public List<Comentario> getComentariosVehiculo(Vehiculo vehiculo){
+
+	/**
+	 * Declaracion del metodo que devuelve una lista de comentarios por vehiculo
+	 * 
+	 * @param vehiculo recibe el objeto
+	 * @return devuelve la lista
+	 */
+	public List<Comentario> getComentariosVehiculo(Vehiculo vehiculo) {
 		return comentarioDAO.getcomentariosVehiculo(vehiculo);
 	}
 }
