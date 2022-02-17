ef0a3b7fc464626565f16bc4cccb2e75b1b38582
braulio97x <52545107+braulio97x@users.noreply.github.com>
2022-02-17 05:15:48 -0500

    ff

--- AlquilerAutoServer/src/main/java/ups/edu/ec/AlquilerAutoServer/services/ContratoTemp.java
+++ AlquilerAutoServer/src/main/java/ups/edu/ec/AlquilerAutoServer/services/ContratoTemp.java
@@ -1,6 +1,14 @@
 package ups.edu.ec.AlquilerAutoServer.services;
 
+/**
+ * Clase que sive como mensaje para las respuetas de los servicios
+ * 
+ * @author Bryan Mendez
+ * @author Braulio Astudillo
+ * @author Juan Boni
+ *
+ */
 public class ContratoTemp {
 
-	private String mensaje;
+	private String mensaje; // mensaje de confirmacion de los servicios
 }
